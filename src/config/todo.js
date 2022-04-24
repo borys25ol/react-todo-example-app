6c7609c7e3b493e078328fdd1ce1f8e6ac26394d
BorysOl <oleynik.boris@gmail.com>
2022-04-24 13:11:47 +0300

    Add runtime environment variables

--- src/config/todo.js
+++ src/config/todo.js
@@ -1,4 +1,8 @@
-const BASE_API_URL = `${process.env.REACT_APP_BASE_API_URL}/api/v1`
+const API_ENV_URL =
+  (window.__RUNTIME_CONFIG__ && window.__RUNTIME_CONFIG__.REACT_APP_BASE_API_URL) ||
+  process.env.REACT_APP_BASE_API_URL
+
+const BASE_API_URL = `${API_ENV_URL}/api/v1`
 
 export const GET_TODOS_URL = `${BASE_API_URL}/tasks`
 export const CREATE_TODO_URL = `${BASE_API_URL}/tasks`
